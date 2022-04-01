300dfcf55311bddd3866675b0c230840c699f4e9
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-04-01 13:38:14 +0200

    datafixtures + faker

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($usr = 0; $usr <= 10; $usr++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+}
